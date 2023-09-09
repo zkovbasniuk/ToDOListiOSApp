fd8ed8ba88e388a86cc7087f42e19770386e352f
zkovbasniuk <zchampion48@gmail.com>
2023-09-09 19:05:23 -0400

    Created register view, made header values generic, finalized login view

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -16,7 +16,7 @@ struct LoginView: View {
         NavigationView {
             VStack {
                 // Header
-                HeaderView()
+                HeaderView(title: "To Do List", subtitle: "Get things done", angle: 15, background: .pink)
                 // Login Form
                 Form {
                     TextField("Email Address", text: $email)
@@ -36,6 +36,7 @@ struct LoginView: View {
                     }
                     .padding()
                 }
+                .offset(y: -50)
                 
                 // Create Account
                 VStack {
