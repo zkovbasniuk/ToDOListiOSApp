62f564d54adbf7913be160e0937936c5ea436dc8
Zorian Kovbasniuk <zoriankovbasniuk@zorians-mbp.lan>
2023-09-16 19:24:10 -0400

    Added functionality to allow user to save task along with due date and time

--- ToDoList/ViewModels/ToDoListViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListViewViewModel.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 class ToDoListViewViewModel: ObservableObject {
+    @Published var showingNewItemView = false
     init() {}
 }
--- ToDoList/Views/NewItemView.swift
+++ ToDoList/Views/NewItemView.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct NewItemView: View {
+    @StateObject var viewModel = NewItemViewViewModel()
+    @Binding var newItemPresented: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("New Item")
+                .bold()
+                .font(.system(size: 32))
+                .padding(.top, 100)
+            
+            Form {
+                //Title
+                TextField("Title", text: $viewModel.title)
+                //Due Date
+                DatePicker("Due Date", selection: $viewModel.dueDate)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .textFieldStyle(DefaultTextFieldStyle())
+                //Button
+                TDLButton(title: "Save", background: .pink) {
+                    if viewModel.canSave {
+                        viewModel.save()
+                        newItemPresented = false
+                    } else {
+                        viewModel.showAlert = true
+                    }
+                }
+                .padding()
+            }
+            .alert(isPresented: $viewModel.showAlert) {
+                Alert(title: Text("Error"), message: Text("Please fill in all fields and select due date.")
+                )
+            }
+        }
     }
 }
 
 struct NewItemView_Previews: PreviewProvider {
     static var previews: some View {
-        NewItemView()
+        NewItemView(newItemPresented: Binding(get: {
+            return true
+        }, set: { _ in
+            
+        }))
     }
 }
