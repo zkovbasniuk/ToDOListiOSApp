fe7e92a8e32a7f21bf89f4e8419fb13d5e2439fb
Zorian Kovbasniuk <zoriankovbasniuk@zorians-mbp.lan>
2023-09-23 19:10:04 -0400

    Check mark functionality is implemented and integrated with completion status in task database

--- ToDoList/Views/ToDoListItemView.swift
+++ ToDoList/Views/ToDoListItemView.swift
@@ -26,6 +26,7 @@ struct ToDoListItemView: View {
                 viewModel.toggleIsDone(item: item)
             } label: {
                 Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
+                    .foregroundColor(Color.blue)
             }
         }
     }
